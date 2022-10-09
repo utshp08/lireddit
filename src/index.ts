5bd3a1b97d9c9f97ee88167851460af2b11c1fdb
Reymart <utspantonia@gmail.com>
2022-10-09 11:03:50 +0800

    Setup mikro orm

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("hello world");
+import { MikroORM } from "@mikro-orm/core";
+import { Post } from "./entities/Post";
+import mikroConfig from "./mikro-orm.config";
+
+const main = async () => {
+    const orm  = await MikroORM.init(mikroConfig);
+    await orm.getMigrator().up();
+
+    // const post = orm.em.create(Post, { title: "Sample post 3"}) // create a Post instance
+    // await orm.em.persistAndFlush(post); // insert the instance to database table
+
+    // const post = await orm.em.find(Post, {id: 4});
+    // console.log(post)
+
+}
+
+main().catch(err => {
+    console.error(err)
+});
